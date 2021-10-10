45359b8192dc6778f4e65d2269df803b8349c82f
Buchholz <manbu@mail.de>
2021-10-10 15:07:32 +0200

    map https

--- addons/--Plugin_Examples/01_Map.php
+++ addons/--Plugin_Examples/01_Map.php
@@ -7,8 +7,8 @@ class Example_Map{
 		global $page, $addonRelativeCode;
 
 		//add css and js to <head>
-		$page->head .= '<link href="http://code.google.com/apis/maps/documentation/javascript/examples/default.css" rel="stylesheet" type="text/css" />';
-		$page->head .= '<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&language=en"></script>';
+		$page->head .= '<link href="https://code.google.com/apis/maps/documentation/javascript/examples/default.css" rel="stylesheet" type="text/css" />';
+		$page->head .= '<script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=false&language=en"></script>';
 		$page->head .= '<script type="text/javascript" src="'.$addonRelativeCode.'/static/01_script.js"></script>';
 
 
