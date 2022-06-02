b586a77021501f401a09895604f7ba0bbeefc0ba
gtbu <regis@vollbio.de>
2022-06-02 20:06:15 +0200

    passing 0 deprecated -corr

--- include/admin/Addon/Installer.php
+++ include/admin/Addon/Installer.php
@@ -1374,7 +1374,7 @@ class Installer extends \gp\admin\Addon\Tools{
 			\gp\tool\Files::RmAll($this->source);
 		}
             
-		if( file_exists($this->trash_path) ?? ''){
+		if( file_exists($this->trash_path ? $this->trash_path : 0) ){
 			\gp\tool\Files::RmAll($this->trash_path);
 		}
 	}
